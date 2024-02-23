77ff9a7e3d51c81978cba97c1a9be5e4867c97dc
Daria Mikhailova <daria@parity.io>
2024-02-23 13:25:51 +1300

    autocenter when a person is chosen from the list of people in the office

--- src/client/components/ui/ImageWithPanZoom.tsx
+++ src/client/components/ui/ImageWithPanZoom.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { cn } from '#client/utils'
 //@ts-ignore
 import { PanZoom, OnStateChangeData } from 'react-easy-panzoom'
@@ -7,6 +7,7 @@ type ImageWithPanZoomProps = {
   src: string
   alt: string
   className?: string
+  initialStartPosition?: { x: number; y: number }
   imageOverlayMappingFn: (scale: number) => Array<React.ReactNode>
   containerClassName?: string
 }
@@ -14,6 +15,7 @@ type ImageWithPanZoomProps = {
 export const ImageWithPanZoom = ({
   src,
   alt,
+  initialStartPosition = { x: 0, y: 0 },
   imageOverlayMappingFn,
   className,
   containerClassName,
@@ -21,6 +23,22 @@ export const ImageWithPanZoom = ({
   const [imgScale, setImgScale] = useState(1)
   const containerRef = useRef(null)
   const imageRef = useRef(null)
+  const panZoomRef = useRef(null)
+
+  const [point, setPoint] = useState(initialStartPosition)
+
+  useEffect(() => {
+    if (
+      point.x !== initialStartPosition.x * imgScale &&
+      point.y !== initialStartPosition.y * imgScale
+    ) {
+      setPoint({
+        x: initialStartPosition.x * imgScale,
+        y: initialStartPosition.y * imgScale,
+      })
+      panZoomRef?.current?.autoCenter()
+    }
+  }, [initialStartPosition])
 
   return (
     <div
@@ -36,18 +54,17 @@ export const ImageWithPanZoom = ({
         minZoom={1}
         autoCenterZoomLevel={2}
         onStateChange={(data: OnStateChangeData) => setImgScale(data.scale)}
+        ref={panZoomRef}
       >
-        <div className="transition-transform touch-none relative">
-          <img
-            ref={imageRef}
-            src={src}
-            alt={alt}
-            className={cn('max-w-none h-auto', className)}
-          />
-          <div className="absolute top-0 left-0 w-full h-full">
-            {/*  passing scale so we can do reverse scaling on the mapped points */}
-            {imageOverlayMappingFn(imgScale)}
-          </div>
+        <img
+          ref={imageRef}
+          src={src}
+          alt={alt}
+          className={cn('max-w-none h-auto', className)}
+        />
+        <div className="absolute top-0 left-0 w-full h-full">
+          {/*  passing scale so we can do reverse scaling on the mapped points */}
+          {imageOverlayMappingFn(imgScale)}
         </div>
       </PanZoom>
     </div>
